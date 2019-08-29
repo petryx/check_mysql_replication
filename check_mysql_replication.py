0519d91bd22deac3388fd8c7525a9a37c370e666
root <root@NlBase>
2019-08-29 00:37:11 -0300

    Fixes

--- check_mysql_replication.py
+++ check_mysql_replication.py
@@ -8,10 +8,11 @@ from email import encoders
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+import traceback
+import ast
 
 config = configparser.ConfigParser()
-config.read('./config.ini')
+config.read('config.ini')
 
 #master config
 master_config = config['master_host']
@@ -32,7 +33,7 @@ slave_database = slave_config['database']
 notify_config = config['notify']
 SUBJECT = notify_config['subject']
 EMAIL_FROM = notify_config['email_from'] 
-EMAIL_TO = notify_config['email_to']
+EMAIL_TO = ast.literal_eval(notify_config['email_to'])
 HOST = notify_config['host']
 PORT = notify_config['port']
 
@@ -59,13 +60,7 @@ def notify_byemail(subject,msg):
         message['From'] = EMAIL_FROM
         message['To'] = ', '.join(EMAIL_TO)
         message.attach(MIMEText(msg, 'plain'))
-        filename = 'report_'+str(time.strftime("%d-%m-%Y"))+'.log'
-        attachment = open(os.path.join(parent_dir,filename), "rb")
         part = MIMEBase('application', 'octet-stream')
-        part.set_payload((attachment).read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-        message.attach(part)
         s = smtplib.SMTP(host=HOST,port=PORT)
         s.sendmail(EMAIL_FROM, EMAIL_TO, message.as_string())
         s.quit()
@@ -99,7 +94,7 @@ log.setLevel(logging.DEBUG)
 
 log.debug("Master: %s %s %s" % (master_ip,master_user,master_passwd))
 log.debug("Slave: %s %s %s" % (slave_ip,slave_user,slave_passwd))
-log.debug("Notify: %s" % (emails))
+log.debug("Notify: %s %s" %(EMAIL_TO, type(EMAIL_TO)))
 
 def connect_mysql(ip,user,password,database):
     try:
@@ -110,14 +105,14 @@ def connect_mysql(ip,user,password,database):
 
 def mysql_query(conn, query):
     try:
-	    cur = conn.cursor(MySQLdb.cursors.DictCursor)
-	    cur.execute(query)
-	    return cur
+        cur = conn.cursor(mdb.cursors.DictCursor)
+        cur.execute(query)
+        return cur
     except Exception as e:
         raise Exception("Execute Mysql Query\n"+str(e))
 
 def verify_slave_stats(conn):
-	try:
+    try:
         result = mysql_query(conn, 'SHOW SLAVE STATUS')
         slave_row = result.fetchone()
         if slave_row is None:
@@ -131,19 +126,20 @@ def verify_slave_stats(conn):
         }
 
         if slave_row['Slave_IO_Running'] != 'Yes':
-            raise Exception('O processo Slave_IO_Running está com algum problema.\nSlave_IO_Running = ' +slave_row['Slave_IO_Running'])
+            raise Exception('O processo Slave_IO_Running está com algum problema.\nSlave_IO_Running = ' +str(slave_row['Slave_IO_Running']))
         if slave_row['Slave_SQL_Running'] != 'Yes':
-            raise Exception('O processo Slave_SQL_Running está com algum problema.\nSlave_SQL_Running = ' +slave_row['Slave_SQL_Running'])
-        if slave_row['Last_IO_Errno'] != '0' or slave_row['Last_IO_Error'] != "":
-            raise Exception('O processo Last_IO_Errno está com algum problema.\nLast_IO_Errno = ' +slave_row['Last_IO_Errno']+"\nLast_IO_Error="+slave_row['Last_IO_Errno'])
-        if slave_row['Last_SQL_Errno'] != '0' or slave_row['Last_SQL_Error'] != "":
-            raise Exception('O processo Last_IO_Errno está com algum problema.\nLast_SQL_Errno = ' +slave_row['Last_SQL_Errno']+"\nLast_SQL_Error="+slave_row['Last_SQL_Error'])
+            raise Exception('O processo Slave_SQL_Running está com algum problema.\nSlave_SQL_Running = ' +str(slave_row['Slave_SQL_Running']))
+        if slave_row['Last_IO_Errno'] != 0 or slave_row['Last_IO_Error'] != "":
+            raise Exception('O processo Last_IO_Errno está com algum problema.\nLast_IO_Errno = ' +str(slave_row['Last_IO_Errno'])+"\nLast_IO_Error="+str(slave_row['Last_IO_Errno']))
+        if slave_row['Last_SQL_Errno'] != 0 or slave_row['Last_SQL_Error'] != "":
+            raise Exception('O processo Last_IO_Errno está com algum problema.\nLast_SQL_Errno = ' +str(slave_row['Last_SQL_Errno'])+"\nLast_SQL_Error="+str(slave_row['Last_SQL_Error']))
         if slave_row['Slave_SQL_Running_State'] != "Slave has read all relay log; waiting for more updates":
-            raise Exception('O processo Slave_SQL_Running_State está com algum problema.\nSlave_SQL_Running_State = ' +slave_row['Slave_SQL_Running_State'])
+            raise Exception('O processo Slave_SQL_Running_State está com algum problema.\nSlave_SQL_Running_State = ' +str(slave_row['Slave_SQL_Running_State']))
+        
         
         return status
     except Exception as error:
-        raise Exception('Function verify_slave_stats\nMensagem de erro:\n'+str(error)))
+        raise Exception('Function verify_slave_stats\nMensagem de erro:\n'+str(error))
 
 def verify_status_master(conn):
     try:
@@ -153,8 +149,8 @@ def verify_status_master(conn):
             raise Exception('A verificação de status do Master retornou vazia.\n Verifique o localmento o banco de dados')
       
         status ={
-            'file': master_row['File']
-            'position': master_row['Position']
+            'file': master_row['File'],
+            'position': master_row['Position'],
             
         }
         return status
@@ -176,5 +172,10 @@ def main():
             raise Exception('A replicação não está funcionando corretamente.\n'+'Position Slave: '+status_slave['position_read_master']+ " Position Master: "+status_master['position'])            
     
     except Exception as error:
+        traceback.print_stack()
+        log.debug('Error '+ str(error))
         notify_byemail('Replicação com Problema',str(error))            
 
+
+if __name__ == '__main__':
+    main()
